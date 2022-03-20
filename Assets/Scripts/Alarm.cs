2313a2499f6abff14f02ce1d161ae7c520e42d23
Sta2yan <s2_stas@mail.ru>
2022-03-20 19:34:41 +0300

    1

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -2,55 +2,47 @@ using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-[RequireComponent (typeof(Door))]
 public class Alarm : MonoBehaviour
 {
     [SerializeField] private float _soundChangeInSecond;
 
     private AudioSource _source;
-    private Door _door;
     private float _maximumValue = 1f;
     private float _minimumValue = 0f;
     private float _startVolume = 0f;
     private float _stepChangeTime;
-    private float _percentOfStep = 100f;
+    private float _percentOfStep = 1000f;
     private WaitForSeconds _waitSeconds;
-    private Coroutine _pastCoroutineChangeSmoothVolume;
+    private Coroutine _coroutine;
 
     private void Awake()
     {
         _source = GetComponent<AudioSource>();
-        _door = GetComponent<Door>();
         _source.Play();
         _source.volume = _startVolume;
         _stepChangeTime = _soundChangeInSecond / _percentOfStep;
         _waitSeconds = new WaitForSeconds(_stepChangeTime);
     }
 
-    public void StartChangeSmoothVolume()
+    public void StartChangeSmoothVolume(int duration)
     {
-        if (_pastCoroutineChangeSmoothVolume != null)
-            StopCoroutine(_pastCoroutineChangeSmoothVolume);
-        _pastCoroutineChangeSmoothVolume = StartCoroutine(ChangeSmoothVolume());
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeSmoothVolume(duration));
     }
 
-    private IEnumerator ChangeSmoothVolume()
+    private IEnumerator ChangeSmoothVolume(int duration)
     {
-        if (_door.HasEntered)
+        _startVolume = _source.volume;
+        float a = 0;
+
+        while (_startVolume <= _maximumValue && _startVolume >= _minimumValue)
         {
-            while (_startVolume <= _maximumValue)
-            {
-                _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume += _soundChangeInSecond * Time.deltaTime);
-                yield return _waitSeconds;
-            }
-        }
-        else
-        {
-            while (_startVolume >= _minimumValue)
-            {
-                _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume -= _soundChangeInSecond * Time.deltaTime);
-                yield return _waitSeconds;
-            }
+            a += Time.deltaTime;
+            _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume += _soundChangeInSecond * Time.deltaTime * duration);
+            Debug.Log(a);
+            yield return _waitSeconds;
         }
     }
 }
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,24 +4,17 @@ using UnityEngine.Events;
 public class Door : MonoBehaviour
 {
     [SerializeField] private UnityEvent _thiefCame = new UnityEvent();
-
-    public bool HasEntered { get; private set; }
+    [SerializeField] private UnityEvent _thiefGone = new UnityEvent();
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<MovementThief>(out MovementThief thief))
-        {
-            HasEntered = true;
             _thiefCame.Invoke();
-        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.TryGetComponent<MovementThief>(out MovementThief thief))
-        {
-            HasEntered = false;
-            _thiefCame.Invoke();
-        }
+            _thiefGone.Invoke();
     }
 }
