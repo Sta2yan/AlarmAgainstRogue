b512b76bc636cbd854e1b6efb1aef01fbf9d9230
Sta2yan <s2_stas@mail.ru>
2022-03-20 13:19:15 +0300

    1

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -2,54 +2,55 @@ using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent (typeof(Door))]
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private float _stepVolumeUp;
+    [SerializeField] private float _soundChangeInSecond;
 
     private AudioSource _source;
+    private Door _door;
     private float _maximumValue = 1f;
     private float _minimumValue = 0f;
     private float _startVolume = 0f;
+    private float _stepChangeTime;
     private float _percentOfStep = 100f;
     private WaitForSeconds _waitSeconds;
+    private Coroutine _pastCoroutineChangeSmoothVolume;
 
     private void Awake()
     {
         _source = GetComponent<AudioSource>();
-        _source.volume = _startVolume;
-        _stepVolumeUp /= _percentOfStep;
-        _waitSeconds = new WaitForSeconds(_stepVolumeUp);
-    }
-
-    public void StartTurnUpVolume()
-    {
-        StartCoroutine(TurnUpVolume());
-    }
-
-    public void StartTurnDownVolume()
-    {
-        StartCoroutine(TurnDownVolume());
-    }
-
-    private IEnumerator TurnUpVolume()
-    {
+        _door = GetComponent<Door>();
         _source.Play();
-
-        while (_startVolume <= _maximumValue)
-        {
-            _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume += _stepVolumeUp);
-            yield return _waitSeconds;
-        }
+        _source.volume = _startVolume;
+        _stepChangeTime = _soundChangeInSecond / _percentOfStep;
+        _waitSeconds = new WaitForSeconds(_stepChangeTime);
     }
 
-    private IEnumerator TurnDownVolume()
+    public void StartChangeSmoothVolume()
     {
-        while (_startVolume >= _minimumValue)
-        {
-            _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume -= _stepVolumeUp);
-            yield return _waitSeconds;
-        }
+        if (_pastCoroutineChangeSmoothVolume != null)
+            StopCoroutine(_pastCoroutineChangeSmoothVolume);
+        _pastCoroutineChangeSmoothVolume = StartCoroutine(ChangeSmoothVolume());
+    }
 
-        _source.Stop();
+    private IEnumerator ChangeSmoothVolume()
+    {
+        if (_door.HasEntered)
+        {
+            while (_startVolume <= _maximumValue)
+            {
+                _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume += _soundChangeInSecond * Time.deltaTime);
+                yield return _waitSeconds;
+            }
+        }
+        else
+        {
+            while (_startVolume >= _minimumValue)
+            {
+                _source.volume = Mathf.MoveTowards(_minimumValue, _maximumValue, _startVolume -= _soundChangeInSecond * Time.deltaTime);
+                yield return _waitSeconds;
+            }
+        }
     }
 }
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,17 +4,24 @@ using UnityEngine.Events;
 public class Door : MonoBehaviour
 {
     [SerializeField] private UnityEvent _thiefCame = new UnityEvent();
-    [SerializeField] private UnityEvent _thiefGone = new UnityEvent();
+
+    public bool HasEntered { get; private set; }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<MovementThief>(out MovementThief thief))
+        {
+            HasEntered = true;
             _thiefCame.Invoke();
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.TryGetComponent<MovementThief>(out MovementThief thief))
-            _thiefGone.Invoke();
+        {
+            HasEntered = false;
+            _thiefCame.Invoke();
+        }
     }
 }
